4c1ee5172770a72aaef3e3942eea426bf1ca2747
Lemuel Ayala <lemuayala@gmail.com>
2025-04-01 18:44:34 -0300

    Se agrega traduccion para la pagina de About

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,8 +1,32 @@
+import { Trans } from 'react-i18next';
+import { useLanguage } from '../../context/LanguageContext';
 import { RevealOnScroll } from '../RevealOnScroll';
 
 export const About = () => {
-  const frontendSkills = ['React', 'Vue', 'Next.js', 'Tailwind CSS'];
-  const backendSkills = ['Node.js', 'Express.js', 'MongoDB', 'NET Core'];
+  const frontendSkills = [
+    'Angular',
+    'React',
+    'TypeScript',
+    'JavaScript',
+    'HTML5',
+    'CSS3',
+    'Material UI',
+    'Bootstrap',
+    'JQuery',
+    'Tailwind CSS',
+  ];
+  const backendSkills = [
+    '.NET Core',
+    '.NET Framework',
+    'ASP.NET MVC',
+    'Web API',
+    ' RESTful APIs',
+    'Entity Framework',
+    'LINQ',
+    'SQL Server',
+  ];
+
+  const { t } = useLanguage();
 
   return (
     <section
@@ -10,23 +34,20 @@ export const About = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
+        <div className="max-w-4xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            About Me
+            {t('about.about')}
           </h2>
           <div className="rounded-xl p-8 border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-600 dark:text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable and
-              efficient web applications. Skilled in using modern technologies
-              such as React, Next.js, and Tailwind CSS to create visually
-              appealing and user-friendly interfaces. Strong problem-solving
-              skills and a passion for continuous learning make me a valuable
-              asset to any team.
+              <Trans i18nKey="about.description"></Trans>
             </p>
 
             <div className="grid grid-cols-2 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-l transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <h3 className="text-xl font-bold mb-4">
+                  {t('about.frontend')}
+                </h3>
                 <div className="flex flex-wrap gap-2">
                   {frontendSkills.map((skill) => (
                     <span
@@ -40,7 +61,7 @@ export const About = () => {
               </div>
 
               <div className="rounded-xl p-6 hover:-translate-y-l transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
+                <h3 className="text-xl font-bold mb-4">{t('about.backend')}</h3>
                 <div className="flex flex-wrap gap-2">
                   {backendSkills.map((skill) => (
                     <span
@@ -55,11 +76,17 @@ export const About = () => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
               <div className="p-6 rounded-xl border-black/10 dark:border-white/10 border hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4">Education</h3>
+                <h3 className="text-xl font-bold mb-4">
+                  {t('about.education.education')}
+                </h3>
                 <ul className="list-disc list-inside text-gray-600 dark:text-gray-300 space-x-2">
                   <li className="mb-2">
-                    <strong> Analista Progrmador Universitario </strong> -
-                    Universidad Nacional de Jujuy (Argentina)
+                    <strong> {t('about.education.apu')} </strong> -{' '}
+                    {t('about.education.universidad')} 2021
+                  </li>
+                  <li className="mb-2">
+                    <strong> {t('about.education.tecnico')} </strong> -{' '}
+                    {t('about.education.edm')}
                   </li>
                   <li className="mb-2">
                     Relevant Coursework - Programming Languages, Databases,
