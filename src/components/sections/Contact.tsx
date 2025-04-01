aa9d6f9a94f5b4005b424a322e0bd197b523baa9
Lemuel Ayala <lemuayala@gmail.com>
2025-04-01 18:44:03 -0300

    Fix estilos en contact para ser responsive

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -62,7 +62,7 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 w-auto md:w-150">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
             Get in Touch
           </h2>
